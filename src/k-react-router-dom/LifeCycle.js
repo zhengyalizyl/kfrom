9d35bf94275cc3ec82c5780811222fa52ca4b023
lizheng <v-lizheng@expedia.com>
2020-09-13 20:00:09 +0800

    fix  bug for lifeCycle

--- src/k-react-router-dom/LifeCycle.js
+++ src/k-react-router-dom/LifeCycle.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class LifeCycle extends Component {
   componentDidMount(){
-    this.props.onMount&&this.props.onMount.call(this)
+    this.props.onMount&&this.props.onMount.call(this,this)
   }
   render() {
     return null;
